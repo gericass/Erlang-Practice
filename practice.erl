572bb5327dd93c9a1fc717ba44c02fc0a7138cd1
Keita <bonjovi1926@yahoo.co.jp>
2018-02-05 22:01:23 +0900

    guard

--- practice.erl
+++ practice.erl
@@ -1,6 +1,13 @@
 -module(practice).
--export([hello/0]).
+-export([hello/0,right_age/1]).
 
 hello() ->
 	io:format("Hello world").
+
+right_age(X) when X >= 16, X =< 104 ->
+true;
+right_age(X) when X < 16; X > 104 ->
+true;
+right_age(_) ->
+false.
 
